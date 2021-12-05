9e92d2c97ed9c68d0d3b931f02f71074d3d52db3
alextheloser <alessiolargiubuz@gmail.com>
2021-12-05 17:43:16 +0100

    Test del commit

---
+++ engine.c
@@ -0,0 +1,5 @@
+//
+// Created by alessio on 05/12/21.
+//
+
+#include "engine.h"
---
+++ engine.h
@@ -0,0 +1,11 @@
+#ifndef SO_BASATO_ENGINE_H
+#define SO_BASATO_ENGINE_H
+
+typedef struct{
+    int x;
+    int y;
+    char * sprite;
+}Position;
+
+
+#endif //SO_BASATO_ENGINE_H
